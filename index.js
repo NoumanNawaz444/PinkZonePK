054e3c65a9a599f0dabcba0a598e59e4d19e9aaa
NoumanNawaz444 <noumannawazwork@gmail.com>
2023-09-15 11:39:38 +0500

    initial changes

---
+++ index.js
@@ -0,0 +1,10 @@
+let hamburger = document.getElementById('ham');
+
+
+hamburger.addEventListener('click' , navBar);
+
+let NAVbAR = document.getElementById('NAV2');
+
+function navBar(){
+   NAVbAR.classList.toggle('navaya')
+}
